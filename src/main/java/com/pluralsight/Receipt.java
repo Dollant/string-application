a01a7e7673b7459c42b443d158ed95ca7fadd896
Dollant <ariawilliams2425@gmail.com>
2026-04-07 14:35:10 -0400

    Created a VariableApp and Receipt class to answer a series of questions and identifying different data types writing code.

--- src/main/java/com/pluralsight/Receipt.java
+++ src/main/java/com/pluralsight/Receipt.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
 public class Receipt {
+    public static void main(String[] args) {
+        // 1. Item name and price
+        String itemName = "apples";
+        double itemPrice = 7.10;
+
+        // 2. Quantity
+        int quantity = 3;
+
+        // 3. Print the receipt message
+        System.out.println("You bought " + quantity + " " + itemName + " for $" + itemPrice);
+
+
+
+
+
+
+    }
 }
--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,65 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+        // ===== STEP 1 =====
+//  Question 1: Favorite Color
+        String favoriteColor;
+        favoriteColor = "Purple";
+        System.out.println("My favorite color is " + favoriteColor);
+
+//   Question 2: Year you started this class
+        int yearStarted;
+        yearStarted = 2026;
+        System.out.println("I started this class in " + yearStarted);
+
+//   Question 3: Middle Initial
+        char middleInitial;
+        middleInitial = 'A';
+        System.out.println("My middle initial is " + middleInitial);
+
+//   Question 4: Do you have pets (Y/N)?
+        String hasPets;
+        hasPets = "NO";
+        System.out.println("Do I have pets? " + hasPets);
+
+//   Question 5: A nice message or saying
+        String niceMessage;
+        niceMessage = "Hope you have a wonderful Tuesday! :)";
+        System.out.println(niceMessage);
+
+        // ===== STEP 2 =====
+
+        // 1-2: Days in a week
+        int daysInAWeek = 7;
+
+        // 3: Price of coffee
+        double coffeePrice = 4.99;
+
+        // 4: Favorite letter
+        char favoriteLetter = 'A';
+
+        // 5: Whether it's raining
+        boolean isRaining = false;
+
+        // 6: Print all values in complete sentences
+        System.out.println("There are " + daysInAWeek + " days in a week.");
+        System.out.println("The price of coffee is $" + coffeePrice);
+        System.out.println("My favorite letter is " + favoriteLetter);
+        System.out.println("Is it raining? " + isRaining);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
